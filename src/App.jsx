4ded51b115d125d51e1c2c04e6c4f803d4e436c5
prachijain <prachijain.work@gmail.com>
2024-06-07 15:10:26 +0200

    added json file

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Contact from "./pages/contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Cover from "./components/Cover";
 import BirthdayCardRow from "./components/BirthdayCardRow";
 import BabyShowerCardRow from "./components/BabyShowerCardRow";
@@ -16,50 +16,17 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route exact path="/" component={<Home />} />
-          <Route path="/about" component={<About />} />
-          <Route path="/contact" component={<Contact />} />
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
+
       <div>
-        <div>
-          <Cover />
-        </div>
-        <div className="listing-title">
-          <h4
-            className="listing-heading"
-            style={{
-              textAlign: "center",
-              paddingTop: "50px",
-              color: "rgb(148, 132, 98)",
-            }}
-          >
-            Birthday Invitations
-          </h4>
-        </div>
-        <div>
-          <BirthdayCardRow />
-        </div>
-        <div className="listing-title">
-          <h4
-            className="listing-heading"
-            style={{
-              textAlign: "center",
-              paddingTop: "80px",
-              color: "rgb(148, 132, 98)",
-            }}
-          >
-            Baby Shower Invitations
-          </h4>
-        </div>
-        <div>
-          <BabyShowerCardRow />
-        </div>
-        <div>
-          <hr />
-          <Footer />
-        </div>
+        <hr />
+        <Footer />
       </div>
+
     </Router>
   );
 }
