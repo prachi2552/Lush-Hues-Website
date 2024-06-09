06d8bb7ec6ff698536df9ed8d6f389237f736471
prachijain <prachijain.work@gmail.com>
2024-06-09 02:39:46 +0200

    added some styles to CardDetails

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Cover from "./components/Cover";
 import CardDetails from "./components/CardDetails";
 import Footer from "./components/Footer";
 
--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -13,8 +13,8 @@ const CardDetails = () => {
     <>
       {cardDetail.img && (
         <div className={`${styles.cardDetailsContainer}`}>
-          <div className="row">
-            <div className="col-md-6">
+          <div className={`row ${styles.cardDetail}`}>
+            <div className={`col-md-6 ${styles.imageContainer}`}>
               <img
                 src={cardDetail.img}
                 alt={cardDetail.title}
@@ -23,42 +23,70 @@ const CardDetails = () => {
             </div>
             <div className="col-md-6">
               <div className="row">
-                <div className="col-md-12">
+                <div className={`col-md-12 ${styles.infoContainer}`}>
+                  <hr />
                   {cardDetail.title && (
-                    <div>
-                      <h3>{cardDetail.title}</h3>
+                    <div className={`${styles.cardDetailTitle}`}>
+                      <h1>{cardDetail.title}</h1>
                     </div>
                   )}
+                  <br />
                   {cardDetail.rating && <div>{cardDetail.rating}</div>}
+                  <br />
                   {cardDetail.price && <div>{cardDetail.price}</div>}
+                  <br />
                   {cardDetail.description && (
                     <div>{cardDetail.description}</div>
                   )}
                   {cardDetail.size && <div>{cardDetail.size}</div>}
+                  <br />
                   <div>
                     <h4>HOW IT WORKS </h4>
-                    <p>
-                      1. After purchase, you will receive an email with a PDF
-                      attachment containing links to your templates. 2. Sign up
-                      or log in to Canva using the link in the PDF. 3. Customize
-                      your template (text, fonts, colors) but not the size. 4.
-                      Save and download as PDF or JPG. 5. Print at home or a
-                      print shop, or share via text, email, or social media.
-                    </p>
+                    <ol>
+                      <li>
+                        After purchase, you will receive an email with a PDF
+                        attachment containing links to your templates.
+                      </li>
+                      <li>
+                        Sign up or log in to Canva using the link in the PDF.
+                      </li>
+                      <li>
+                        Customize your template (text, fonts, colors) but not
+                        the size.
+                      </li>
+                      <li>Save and download as PDF or JPG.</li>
+                      <li>
+                        Print at home or a print shop, or share via text, email,
+                        or social media.
+                      </li>
+                    </ol>
                   </div>
+                  <br />
                   <div>
                     <h4>PLEASE NOTE</h4>
+                    <ul>
+                      <li>
+                        The colors you see on your computer screen may differ
+                        from the printed version due to your computer and
+                        printing settings.
+                      </li>
+                      <li>
+                        We recommend printing on heavy card stock (240 - 290
+                        gsm) for best results.
+                      </li>
+                      <li>
+                        If you have any questions, requests, or problems, please
+                        feel free to contact us anytime. We will get back to you
+                        within 24 hours.
+                      </li>
+                    </ul>
+
                     <p>
-                      • The colors you see on your computer screen may differ
-                      from the printed version due to your computer and printing
-                      settings. • We recommend printing on heavy card stock (240
-                      - 290 gsm) for best results. • If you have any questions,
-                      requests, or problems, please feel free to contact us
-                      anytime. We will get back to you within 24 hours. * * The
-                      template is for personal use only and not for commercial
-                      use. * *
+                      * * The template is for personal use only and not for
+                      commercial use. * *
                     </p>
                   </div>
+                  <br />
                   <div>
                     <h4>RETURNS</h4>
                     <p>
